ab1f3ccdd3876a75e03c871d1fb2e526689ee35d
Nils Nahooy <nils.nahooy@live.com>
2022-09-09 17:32:46 +0200

    Resize brush size dialog implemented

--- app/src/main/java/com/nilsnahooy/drawingapp/CanvasView.kt
+++ app/src/main/java/com/nilsnahooy/drawingapp/CanvasView.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -29,7 +30,6 @@ class CanvasView(context: Context, attrs : AttributeSet): View(context, attrs) {
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.0f
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -86,6 +86,11 @@ class CanvasView(context: Context, attrs : AttributeSet): View(context, attrs) {
         return true
     }
 
+    fun setBrushSize(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+        newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
     internal inner class DrawPath(var color: Int, var brushSize: Float): Path() {
 
     }
--- app/src/main/java/com/nilsnahooy/drawingapp/MainActivity.kt
+++ app/src/main/java/com/nilsnahooy/drawingapp/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.nilsnahooy.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var canvasView: CanvasView? = null
+    private var btnBrushSize: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        canvasView = findViewById(R.id.cv_main_canvas)
+        btnBrushSize = findViewById(R.id.ib_open_brush_size_dialog)
+        canvasView?.setBrushSize(20.0f)
+
+        btnBrushSize?.setOnClickListener {
+            showBrushSizeDialog()
+        }
+    }
+
+    private fun showBrushSizeDialog() {
+        val brushSizeDialog = Dialog(this)
+        brushSizeDialog.setContentView(R.layout.dialog_brush_size)
+        brushSizeDialog.setTitle(R.string.brush_size_dialog_title)
+
+        val btnSmall: ImageButton = brushSizeDialog.findViewById(R.id.ib_brush_size_small)
+        val btnMedium: ImageButton = brushSizeDialog.findViewById(R.id.ib_brush_size_medium)
+        val btnLarge: ImageButton = brushSizeDialog.findViewById(R.id.ib_brush_size_large)
+
+        btnSmall.setOnClickListener {
+           canvasView?.setBrushSize(10f)
+           brushSizeDialog.dismiss()
+       }
+
+        btnMedium.setOnClickListener {
+            canvasView?.setBrushSize(15f)
+            brushSizeDialog.dismiss()
+        }
+
+        btnLarge.setOnClickListener {
+            canvasView?.setBrushSize(20f)
+            brushSizeDialog.dismiss()
+        }
+
+        brushSizeDialog.show()
     }
 }
